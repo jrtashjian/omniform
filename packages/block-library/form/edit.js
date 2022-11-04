0810f1fcfe5d2e81c400c476b079bff8b9b62b27
JR Tashjian <hello@jrtashjian.com>
2022-11-04 00:08:24 -0400

    Make form block like a reusable block

--- packages/block-library/form/edit.js
+++ packages/block-library/form/edit.js
@@ -3,25 +3,103 @@
  */
 import { __ } from '@wordpress/i18n';
 import {
+	useEntityBlockEditor,
+	useEntityProp,
+	useEntityRecord,
+} from '@wordpress/core-data';
+import {
+	__experimentalRecursionProvider as RecursionProvider,
+	__experimentalUseHasRecursion as useHasRecursion,
+	InnerBlocks,
+	InspectorControls,
 	useBlockProps,
 	useInnerBlocksProps,
+	Warning,
 } from '@wordpress/block-editor';
+import {
+	PanelBody,
+	Placeholder,
+	Spinner,
+	TextControl,
+} from '@wordpress/components';
 
-const Edit = () => {
-	const blockProps = useBlockProps();
-	const innerBlockProps = useInnerBlocksProps();
+const Edit = ( { attributes: { ref } } ) => {
+	const hasAlreadyRendered = useHasRecursion( ref );
+	const { record, hasResolved } = useEntityRecord(
+		'postType',
+		'inquirywp_form',
+		ref
+	);
+	const isMissing = hasResolved && ! record;
+
+	const [ blocks, onInput, onChange ] = useEntityBlockEditor(
+		'postType',
+		'inquirywp_form',
+		{ id: ref }
+	);
+	const [ title, setTitle ] = useEntityProp(
+		'postType',
+		'inquirywp_form',
+		'title',
+		ref
+	);
+
+	const blockProps = useBlockProps( {
+		className: 'block-library-block__reusable-block-container',
+	} );
+
+	const innerBlockProps = useInnerBlocksProps( blockProps, {
+		value: blocks,
+		onInput,
+		onChange,
+		renderAppender: blocks?.length
+			? undefined
+			: InnerBlocks.ButtonBlockAppender,
+	} );
+
+	if ( hasAlreadyRendered ) {
+		return (
+			<div { ...blockProps }>
+				<Warning>
+					{ __( 'Form cannot be rendered inside itself.', 'inquirywp' ) }
+				</Warning>
+			</div>
+		);
+	}
+
+	if ( isMissing ) {
+		return (
+			<div { ...blockProps }>
+				<Warning>
+					{ __( 'Form has been deleted or is unavailable.', 'inquirywp' ) }
+				</Warning>
+			</div>
+		);
+	}
+
+	if ( ! hasResolved ) {
+		return (
+			<div { ...blockProps }>
+				<Placeholder>
+					<Spinner />
+				</Placeholder>
+			</div>
+		);
+	}
 
 	return (
-		<form { ...blockProps }>
+		<RecursionProvider uniqueId={ ref }>
+			<InspectorControls>
+				<PanelBody>
+					<TextControl
+						label={ __( 'Name', 'inquirywp' ) }
+						value={ title }
+						onChange={ setTitle }
+					/>
+				</PanelBody>
+			</InspectorControls>
 			<div { ...innerBlockProps } />
-			<button
-				className="form-submit-button"
-				type="submit"
-				disabled
-			>
-				{ __( 'Submit', 'inquirywp' ) }
-			</button>
-		</form>
+		</RecursionProvider>
 	);
 };
 export default Edit;
