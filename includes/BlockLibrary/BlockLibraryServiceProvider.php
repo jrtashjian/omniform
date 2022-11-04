6e3b3d47bd2a508230cd04550975a23fcf3c22ee
JR Tashjian <hello@jrtashjian.com>
2022-11-04 00:34:59 -0400

    Start the basic form render callback

--- includes/BlockLibrary/BlockLibraryServiceProvider.php
+++ includes/BlockLibrary/BlockLibraryServiceProvider.php
@@ -24,7 +24,12 @@ class BlockLibraryServiceProvider extends ServiceProvider {
 	 * Register the blocks.
 	 */
 	public function registerBlocks() {
-		register_block_type( $this->app->basePath( '/packages/block-library/form' ) );
+		register_block_type(
+			$this->app->basePath( '/packages/block-library/form' ),
+			array(
+				'render_callback' => array( $this, 'renderBlockForm' ),
+			)
+		);
 		register_block_type( $this->app->basePath( '/packages/block-library/field-input' ) );
 	}
 
@@ -32,4 +37,43 @@ class BlockLibraryServiceProvider extends ServiceProvider {
 	 * Enqueue required scripts and styles.
 	 */
 	public function register() {}
+
+	/**
+	 * Render the form block.
+	 *
+	 * @param array $attributes The block attributes.
+	 * @return string Rendered HTML of the referenced block.
+	 */
+	public function renderBlockForm( $attributes ) {
+		if ( empty( $attributes['ref'] ) ) {
+			return '';
+		}
+
+		$form_block = get_post( $attributes['ref'] );
+		if ( ! $form_block || 'inquirywp_form' !== $form_block->post_type ) {
+			return '';
+		}
+
+		if ( 'publish' !== $form_block->post_status || ! empty( $form_block->post_password ) ) {
+			return '';
+		}
+
+		$button_classes = array(
+			'wp-block-button',
+			wp_theme_get_element_class_name( 'button' ),
+		);
+		$button_markup = sprintf(
+			'<button type="submit" class="%s">%s</button>',
+			esc_attr( implode( ' ', $button_classes ) ),
+			'Submit'
+		);
+
+		$content = do_blocks( $form_block->post_content );
+
+		return sprintf(
+			'<form method="post" action="%s">%s</form>',
+			esc_url( home_url( '/' ) ),
+			$content . $button_markup
+		);
+	}
 }
