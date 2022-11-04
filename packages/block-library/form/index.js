231ad41ebc9734f44351a9082db5e1dfb6d4cb20
JR Tashjian <hello@jrtashjian.com>
2022-11-04 00:07:42 -0400

    Use __experimentalLabel to set block name to post title

--- packages/block-library/form/index.js
+++ packages/block-library/form/index.js
@@ -2,13 +2,13 @@
  * WordPress dependencies
  */
 import { registerBlockType } from '@wordpress/blocks';
+import { useSelect } from '@wordpress/data';
 
 /**
  * Internal dependencies
  */
 import json from './block.json';
 import Edit from './edit';
-import Save from './save';
 
 import './style.scss';
 import './index.scss';
@@ -17,5 +17,24 @@ const { name } = json;
 
 registerBlockType( name, {
 	edit: Edit,
-	save: Save,
+	// Get block name from the post name.
+	__experimentalLabel: ( attributes ) => {
+		const { ref } = attributes;
+
+		const { value } = useSelect(
+			( select ) => {
+				const { getEntityRecord, getEditedEntityRecord } = select( 'core' );
+
+				const record = getEntityRecord( 'postType', 'inquirywp_form', ref );
+				const editedRecord = getEditedEntityRecord( 'postType', 'inquirywp_form', ref );
+
+				return record && editedRecord
+					? { value: editedRecord.title }
+					: {};
+			},
+			[ ref ]
+		);
+
+		return value;
+	},
 } );
