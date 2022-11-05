69e02ffaba589e09d45939316255ade5af8c6474
JR Tashjian <hello@jrtashjian.com>
2022-11-05 16:53:58 -0400

    Add viewScript for form submission

---
+++ packages/block-library/form/view.js
@@ -0,0 +1,23 @@
+/* global FormData */
+( function() {
+	'use strict';
+
+	document.addEventListener( 'DOMContentLoaded', function() {
+		const formSubmissionHandler = ( event ) => {
+			event.preventDefault();
+
+			const formElement = event.target;
+			const { action, method } = formElement;
+			const body = new FormData( formElement );
+
+			console.debug( { action, method, body } );
+
+			// fetch( action, { method, body } )
+			// 	.then( ( response ) => console.debug( response ) )
+			// 	.catch( ( error ) => console.debug( error ) );
+		};
+
+		document.querySelectorAll( 'form.wp-block-inquirywp-form' )
+			.forEach( ( form ) => form.addEventListener( 'submit', formSubmissionHandler ) );
+	} );
+}() );
