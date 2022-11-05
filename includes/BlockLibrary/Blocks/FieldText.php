07fbac122420578eba6b35a92a714ca39c32b724
JR Tashjian <hello@jrtashjian.com>
2022-11-05 16:21:11 -0400

    Render field-text blocks

--- includes/BlockLibrary/Blocks/FieldText.php
+++ includes/BlockLibrary/Blocks/FieldText.php
@@ -29,6 +29,34 @@ class FieldText implements FormBlockInterface {
 	 * @return string Returns the block content.
 	 */
 	public function renderBlock( $attributes, $content ) {
-		return '<pre>' . print_r( $attributes, true ) . '</pre>';
+		if ( empty( $attributes['label'] ) ) {
+			return '';
+		}
+
+		$field_name = sanitize_title( $attributes['label'] );
+
+		$field_label = sprintf(
+			'<label class="field-label" for="%s">%s</label>',
+			esc_attr( $field_name ),
+			wp_kses_post( $attributes['label'] )
+		);
+
+		$field_control = sprintf(
+			'<input class="field-control" type="%s" placeholder="%s" id="%s" name="%s" />',
+			esc_attr( $attributes['type'] ),
+			empty( $attributes['placeholder'] ) ? '' : esc_attr( $attributes['placeholder'] ),
+			esc_attr( $field_name ),
+			esc_attr( $field_name )
+		);
+
+		$field_help = empty( $attributes['help'] ) ? '' : sprintf(
+			'<p class="field-help">%s</p>',
+			wp_kses_post( $attributes['help'] )
+		);
+
+		return sprintf(
+			'<div class="field-control">%s</div>',
+			$field_label . $field_control . $field_help
+		);
 	}
 }
