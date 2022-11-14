e5feffae35092d32116e88b1c08eb302e0e408e3
JR Tashjian <hello@jrtashjian.com>
2022-11-14 02:08:41 -0500

    re-add input variations

--- packages/block-library/field-input/variations.js
+++ packages/block-library/field-input/variations.js
@@ -29,6 +29,71 @@ const variations = [
 		title: __( 'field-radio', 'inquirywp' ),
 		attributes: { type: 'radio' },
 	},
+	{
+		name: 'field-color',
+		title: __( 'field-color', 'inquirywp' ),
+		attributes: { type: 'color' },
+	},
+	{
+		name: 'field-date',
+		title: __( 'field-date', 'inquirywp' ),
+		attributes: { type: 'date' },
+	},
+	{
+		name: 'field-datetime-local',
+		title: __( 'field-datetime-local', 'inquirywp' ),
+		attributes: { type: 'datetime-local' },
+	},
+	{
+		name: 'field-email',
+		title: __( 'field-email', 'inquirywp' ),
+		attributes: { type: 'email' },
+	},
+	{
+		name: 'field-file',
+		title: __( 'field-file', 'inquirywp' ),
+		attributes: { type: 'file' },
+	},
+	{
+		name: 'field-month',
+		title: __( 'field-month', 'inquirywp' ),
+		attributes: { type: 'month' },
+	},
+	{
+		name: 'field-password',
+		title: __( 'field-password', 'inquirywp' ),
+		attributes: { type: 'password' },
+	},
+	{
+		name: 'field-range',
+		title: __( 'field-range', 'inquirywp' ),
+		attributes: { type: 'range' },
+	},
+	{
+		name: 'field-search',
+		title: __( 'field-search', 'inquirywp' ),
+		attributes: { type: 'search' },
+	},
+	{
+		name: 'field-tel',
+		title: __( 'field-tel', 'inquirywp' ),
+		attributes: { type: 'tel' },
+	},
+	{
+		name: 'field-time',
+		title: __( 'field-time', 'inquirywp' ),
+		attributes: { type: 'time' },
+	},
+	{
+		name: 'field-url',
+		title: __( 'field-url', 'inquirywp' ),
+		attributes: { type: 'url' },
+	},
+	{
+		name: 'field-week',
+		title: __( 'field-week', 'inquirywp' ),
+		attributes: { type: 'week' },
+	},
 ];
 
 variations.forEach( ( variation ) => {
