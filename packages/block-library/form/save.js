1c4b789e7b068d6c44a985ac1b1c146facc3eca2
JR Tashjian <hello@jrtashjian.com>
2022-11-15 23:16:06 -0500

    cleanup

--- packages/block-library/form/save.js
+++
@@ -1,9 +0,0 @@
-/**
- * WordPress dependencies
- */
-import { useInnerBlocksProps, useBlockProps } from '@wordpress/block-editor';
-
-const Save = () => {
-	return <form { ...useInnerBlocksProps.save( useBlockProps.save() ) } />;
-};
-export default Save;
