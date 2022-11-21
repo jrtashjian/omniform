2666be557ac8bb6c559dba7673c23d9d4c0ef618
JR Tashjian <hello@jrtashjian.com>
2022-11-20 21:50:06 -0500

    use const

--- packages/block-library/form/edit/inspector-controls.js
+++ packages/block-library/form/edit/inspector-controls.js
@@ -6,13 +6,18 @@ import { useEntityProp } from '@wordpress/core-data';
 import { TextControl } from '@wordpress/components';
 import { InspectorControls } from '@wordpress/block-editor';
 
+/**
+ * Internal dependencies
+ */
+import { FORM_POST_TYPE } from '../../shared/constants';
+
 export default function FormInspectorControls( {
 	formId,
 	isEntityAvailable,
 } ) {
 	const [ title, setTitle ] = useEntityProp(
 		'postType',
-		'inquirywp_form',
+		FORM_POST_TYPE,
 		'title',
 		formId
 	);
