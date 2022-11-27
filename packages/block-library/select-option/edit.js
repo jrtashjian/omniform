bb1497ec022a8c94acc86781c2e89b756a32cfb5
JR Tashjian <hello@jrtashjian.com>
2022-11-27 00:18:23 -0500

    select-option: enter to create new option

--- packages/block-library/select-option/edit.js
+++ packages/block-library/select-option/edit.js
@@ -1,9 +1,4 @@
 /**
- * External dependencies
- */
-import classNames from 'classnames';
-
-/**
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
@@ -11,11 +6,16 @@ import {
 	RichText,
 	useBlockProps,
 } from '@wordpress/block-editor';
+import { createBlock } from '@wordpress/blocks';
 
 const Edit = ( props ) => {
 	const {
 		attributes,
 		setAttributes,
+		mergeBlocks,
+		onReplace,
+		onRemove,
+		clientId,
 	} = props;
 	const {
 		label,
@@ -26,16 +26,37 @@ const Edit = ( props ) => {
 	} );
 
 	return (
-		<li { ...blockProps }>
-			<RichText
-				aria-label={ __( 'Help text', 'omniform' ) }
-				placeholder={ __( 'Write the option text…', 'omniform' ) }
-				allowedFormats={ [] }
-				withoutInteractiveFormatting
-				value={ label }
-				onChange={ ( html ) => setAttributes( { label: html } ) }
-			/>
-		</li>
+		<RichText
+			{ ...blockProps }
+			tagName="li"
+			aria-label={ __( 'Help text', 'omniform' ) }
+			placeholder={ __( 'Write the option text…', 'omniform' ) }
+			allowedFormats={ [] }
+			withoutInteractiveFormatting
+			value={ label }
+			onChange={ ( html ) => setAttributes( { label: html } ) }
+			onSplit={ ( value, isOriginal ) => {
+				let newAttributes;
+
+				if ( isOriginal || value ) {
+					newAttributes = {
+						...attributes,
+						label: value.trim(),
+					};
+				}
+
+				const block = createBlock( props.name, newAttributes );
+
+				if ( isOriginal ) {
+					block.clientId = clientId;
+				}
+
+				return block;
+			} }
+			onMerge={ mergeBlocks }
+			onReplace={ onReplace }
+			onRemove={ onRemove }
+		/>
 	);
 };
 export default Edit;
