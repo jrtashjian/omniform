fd5eb1062ddef13e29ec064d5207290cca742a0e
JR Tashjian <hello@jrtashjian.com>
2022-12-22 01:12:01 -0500

    Utilize get_block_wrapper_attributes in fieldset

--- includes/BlockLibrary/Blocks/Fieldset.php
+++ includes/BlockLibrary/Blocks/Fieldset.php
@@ -25,35 +25,11 @@ class Fieldset extends BaseBlock {
 			return '';
 		}
 
-		$attributes = array_filter(
-			array(
-				$this->getElementAttribute( 'class', $this->getDefaultClasses() ),
-				$this->getElementAttribute( 'style', $this->getColorStyles( $this->attributes ) ),
-			)
-		);
-
 		return sprintf(
 			'<fieldset %s><legend class="omniform-field-label">%s</legend>%s</fieldset>',
-			trim( implode( ' ', $attributes ) ),
+			get_block_wrapper_attributes(),
 			esc_html( $this->getBlockAttribute( 'fieldLabel' ) ),
 			$this->content
-		);
-	}
-
-	/**
-	 * Get the default classes to be applied to the block wrapper.
-	 *
-	 * @return array
-	 */
-	public function getDefaultClasses() {
-		$default = array(
-			$this->blockTypeClassname(),
-			'is-layout-flow',
-		);
-
-		return array_merge(
-			$default,
-			$this->getColorClasses( $this->attributes ),
 		);
 	}
 }
