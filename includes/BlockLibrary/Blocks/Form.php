49c2556d837bd50fd13d4351ec2e72ea5fa33d27
JR Tashjian <hello@jrtashjian.com>
2022-12-22 02:30:55 -0500

    Submit the form to the /submissions endpoint

--- includes/BlockLibrary/Blocks/Form.php
+++ includes/BlockLibrary/Blocks/Form.php
@@ -43,54 +43,45 @@ class Form extends BaseBlock {
 			return '';
 		}
 
-		$form_classes = array_merge(
-			array(
-				'wp-block-omniform-form',
-				empty( $this->attributes['className'] ) ? '' : esc_attr( $this->attributes['className'] ),
-			),
-			$this->getColorClasses( $this->attributes ),
-		);
+		// // Process the form.
+		// $form_ingestion = omniform()->get( FormIngestionEngine::class );
+		// $form_ingestion->setFormId( $entity_id );
 
-		// Process the form.
-		$form_ingestion = omniform()->get( FormIngestionEngine::class );
-		$form_ingestion->setFormId( $entity_id );
+		// $post_data = '';
 
-		$post_data = '';
+		// if ( $form_ingestion->willProcess() ) {
+		// $post_data = sprintf(
+		// '<pre class="wp-block-preformatted">%s</pre>',
+		// print_r(
+		// array(
+		// 'attributes'     => $this->attributes,
+		// '_POST'          => $_POST,
+		// 'form_ingestion' => $form_ingestion,
+		// ),
+		// true
+		// )
+		// );
 
-		if ( $form_ingestion->willProcess() ) {
-			$post_data = sprintf(
-				'<pre class="wp-block-preformatted">%s</pre>',
-				print_r(
-					array(
-						'attributes'     => $this->attributes,
-						'_POST'          => $_POST,
-						'form_ingestion' => $form_ingestion,
-					),
-					true
-				)
-			);
+		// $form_ingestion->savePostData();
 
-			$form_ingestion->savePostData();
+		// Incremement form submissions.
+		// $submissions = get_post_meta( $entity_id, 'submissions', true );
+		// update_post_meta( $entity_id, 'submissions', (int) $submissions + 1 );
+		// } else {
+		// Incremement form impressions.
+		// $impressions = get_post_meta( $entity_id, 'impressions', true );
+		// update_post_meta( $entity_id, 'impressions', (int) $impressions + 1 );
+		// }
 
-			// Incremement form submissions.
-			$submissions = get_post_meta( $entity_id, 'submissions', true );
-			update_post_meta( $entity_id, 'submissions', (int) $submissions + 1 );
-		} else {
-			// Incremement form impressions.
-			$impressions = get_post_meta( $entity_id, 'impressions', true );
-			update_post_meta( $entity_id, 'impressions', (int) $impressions + 1 );
-		}
-
-		$content = do_blocks( $form_block->post_content );
-		$form_ingestion->resetFormData();
+		$content     = do_blocks( $form_block->post_content );
+		$nonce_field = wp_nonce_field( 'omniform', 'wp_rest', true, false );
+		// $form_ingestion->resetFormData();
 
 		return sprintf(
-			'%s<form method="post" action="%s" %s %s>%s</form>',
-			$post_data,
-			esc_url( get_the_permalink() ),
-			$this->getElementAttribute( 'class', $form_classes ),
-			$this->getElementAttribute( 'style', $this->getColorStyles( $this->attributes ) ),
-			$form_ingestion->getNonceField() . $content
+			'<form method="post" action="%s" %s>%s</form>',
+			esc_url( rest_url( 'omniform/v1/forms/' . $entity_id . '/submissions' ) ),
+			get_block_wrapper_attributes(),
+			$content . $nonce_field
 		);
 	}
 }
