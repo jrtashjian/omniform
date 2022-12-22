73219afd444842538798e213e56ffb5b5e751553
JR Tashjian <hello@jrtashjian.com>
2022-12-22 00:50:48 -0500

    test JS submission

--- packages/block-library/form/view.js
+++ packages/block-library/form/view.js
@@ -12,15 +12,19 @@ import tinycolor from 'tinycolor2';
 			const { action, method } = formElement;
 			const body = new FormData( formElement );
 
-			console.debug( { action, method, body } );
+			const debug = { action, method, data: {} };
+			for ( const pair of body.entries() ) {
+				debug.data[ pair[ 0 ] ] = pair[ 1 ];
+			}
+			console.debug( debug );
 
 			// fetch( action, { method, body } )
 			// 	.then( ( response ) => console.debug( response ) )
 			// 	.catch( ( error ) => console.debug( error ) );
 		};
 
-		// document.querySelectorAll( 'form.wp-block-omniform-form' )
-		// 	.forEach( ( form ) => form.addEventListener( 'submit', formSubmissionHandler ) );
+		document.querySelectorAll( 'form.wp-block-omniform-form' )
+			.forEach( ( form ) => form.addEventListener( 'submit', formSubmissionHandler ) );
 
 		const isDark = ( elm ) => {
 			const context = document.defaultView.getComputedStyle( elm, null );
