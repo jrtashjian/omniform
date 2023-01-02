7cee2c48e53e3451f59ed580f2583dc6d92323f9
JR Tashjian <hello@jrtashjian.com>
2023-01-02 16:58:35 -0500

    Add fieldName to variations

--- packages/block-library/fieldset/variations.js
+++ packages/block-library/fieldset/variations.js
@@ -10,12 +10,14 @@ const variations = [
 		description: __( '', 'omnigroup' ),
 		attributes: {
 			fieldLabel: __( 'Multiple Choice', 'omniform' ),
+			fieldName: 'multiple-choice',
 		},
 		innerBlocks: [
 			{
 				attributes: {
 					fieldType: 'radio',
 					fieldLabel: __( 'Option One', 'omniform' ),
+					fieldName: 'option-one',
 				},
 				name: 'omniform/field-input',
 			},
@@ -23,6 +25,7 @@ const variations = [
 				attributes: {
 					fieldType: 'radio',
 					fieldLabel: __( 'Option Two', 'omniform' ),
+					fieldName: 'option-two',
 				},
 				name: 'omniform/field-input',
 			},
@@ -30,6 +33,7 @@ const variations = [
 				attributes: {
 					fieldType: 'radio',
 					fieldLabel: __( 'Option Three', 'omniform' ),
+					fieldName: 'option-three',
 				},
 				name: 'omniform/field-input',
 			},
@@ -41,21 +45,23 @@ const variations = [
 		description: __( '', 'omnigroup' ),
 		attributes: {
 			fieldLabel: __( 'Full Name', 'omniform' ),
+			fieldName: 'full-name',
 		},
 		innerBlocks: [
 			{
 				attributes: {
 					fieldLabel: __( 'Title', 'omniform' ),
+					fieldName: 'title',
 				},
 				innerBlocks: [
-					[ 'omniform/select-option', { fieldLabel: __( 'Mr.', 'omniform' ) } ],
-					[ 'omniform/select-option', { fieldLabel: __( 'Mrs.', 'omniform' ) } ],
-					[ 'omniform/select-option', { fieldLabel: __( 'Miss', 'omniform' ) } ],
-					[ 'omniform/select-option', { fieldLabel: __( 'Ms.', 'omniform' ) } ],
-					[ 'omniform/select-option', { fieldLabel: __( 'Dr.', 'omniform' ) } ],
-					[ 'omniform/select-option', { fieldLabel: __( 'Prof.', 'omniform' ) } ],
-					[ 'omniform/select-option', { fieldLabel: __( 'Rev.', 'omniform' ) } ],
-					[ 'omniform/select-option', { fieldLabel: __( 'Hon.', 'omniform' ) } ],
+					[ 'omniform/select-option', { fieldLabel: __( 'Mr.', 'omniform' ), fieldName: 'mr.' } ],
+					[ 'omniform/select-option', { fieldLabel: __( 'Mrs.', 'omniform' ), fieldName: 'mrs.' } ],
+					[ 'omniform/select-option', { fieldLabel: __( 'Miss', 'omniform' ), fieldName: 'miss' } ],
+					[ 'omniform/select-option', { fieldLabel: __( 'Ms.', 'omniform' ), fieldName: 'ms.' } ],
+					[ 'omniform/select-option', { fieldLabel: __( 'Dr.', 'omniform' ), fieldName: 'dr.' } ],
+					[ 'omniform/select-option', { fieldLabel: __( 'Prof.', 'omniform' ), fieldName: 'prof.' } ],
+					[ 'omniform/select-option', { fieldLabel: __( 'Rev.', 'omniform' ), fieldName: 'rev.' } ],
+					[ 'omniform/select-option', { fieldLabel: __( 'Hon.', 'omniform' ), fieldName: 'hon.' } ],
 				],
 				name: 'omniform/field-select',
 			},
@@ -63,6 +69,7 @@ const variations = [
 				attributes: {
 					fieldType: 'text',
 					fieldLabel: __( 'First Name', 'omniform' ),
+					fieldName: 'first-name',
 				},
 				name: 'omniform/field-input',
 			},
@@ -70,6 +77,7 @@ const variations = [
 				attributes: {
 					fieldType: 'text',
 					fieldLabel: __( 'Last Name', 'omniform' ),
+					fieldName: 'last-name',
 				},
 				name: 'omniform/field-input',
 			},
