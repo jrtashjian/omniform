50704eef808b1523393bde91f1ea8ac5300f5593
JR Tashjian <hello@jrtashjian.com>
2023-01-08 01:25:45 -0500

    Revert block appender for fieldset

--- packages/block-library/fieldset/edit.js
+++ packages/block-library/fieldset/edit.js
@@ -8,36 +8,24 @@ import { kebabCase } from 'lodash';
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { useSelect } from '@wordpress/data';
 import {
-	InnerBlocks,
 	RichText,
 	useBlockProps,
 	useInnerBlocksProps,
-	store as blockEditorStore,
 } from '@wordpress/block-editor';
 
 const Edit = ( {
 	attributes,
 	setAttributes,
-	isSelected,
-	clientId,
 } ) => {
 	const {
 		fieldLabel,
 		fieldName,
 	} = attributes;
 
-	const hasSelectedInnerBlock = useSelect(
-		( select ) => select( blockEditorStore ).hasSelectedInnerBlock( clientId, true ),
-		[ clientId ]
-	);
-
 	const blockProps = useBlockProps();
 
-	const innerBlockProps = useInnerBlocksProps( {}, {
-		renderAppender: ( isSelected || hasSelectedInnerBlock ) && InnerBlocks.ButtonBlockAppender,
-	} );
+	const innerBlockProps = useInnerBlocksProps();
 
 	return (
 		<div
