5a98bb7fbda404ea9a46352dc984a9902767a8f1
JR Tashjian <hello@jrtashjian.com>
2023-01-23 01:17:59 -0500

    Update unit test suite

--- phpunit/includes/Application.php
+++ phpunit/includes/Application.php
@@ -8,7 +8,6 @@
 namespace OmniForm\Tests\Core;
 
 use OmniForm\Application;
-use OmniForm\ServiceProvider;
 
 /**
  * Tests the Core class.
@@ -52,16 +51,6 @@ class CoreTest extends \WP_UnitTestCase {
 	}
 
 	/**
-	 * Test the contstructor parameter will generate the correct path bindings.
-	 */
-	public function test_core_constructor_can_register_path_bindings() {
-		$application = new Application( $this->plugin_file );
-
-		$this->assertEquals( $this->plugin_path, $application->basePath() );
-		$this->assertEquals( site_url( '/wp-content/plugins/' . basename( $this->plugin_path ) ), $application->baseUrl() );
-	}
-
-	/**
 	 * Test the version is accessible.
 	 */
 	public function test_core_returns_version() {
@@ -73,68 +62,12 @@ class CoreTest extends \WP_UnitTestCase {
 	}
 
 	/**
-	 * Test that a service provider can be registered.
-	 */
-	public function test_core_register_provider() {
-		$application = new Application();
-
-		$application->register( ServiceProviderStub::class );
-
-		$this->assertInstanceOf( ServiceProviderStub::class, $application->getProvider( ServiceProviderStub::class ) );
-	}
-
-	/**
-	 * Test that the provider is returned when registered.
-	 */
-	public function test_core_returns_provider_when_registered() {
-		$application = new Application();
-
-		$this->assertInstanceOf( ServiceProviderStub::class, $application->register( ServiceProviderStub::class ) );
-	}
-
-	/**
-	 * Test that the provider is only registered once.
-	 */
-	public function test_core_register_returns_existing_provider() {
-		$application = new Application();
-
-		$instance = $application->register( ServiceProviderStub::class );
-
-		$this->assertSame( $instance, $application->register( ServiceProviderStub::class ) );
-	}
-
-	/**
-	 * Test that providers can be "booted".
-	 */
-	public function test_core_can_boot_providers() {
-		$application = new Application();
-
-		$application->register( ServiceProviderStub::class );
-		$application->boot();
-
-		$this->expectOutputString( 'booted' );
-	}
-
-	/**
-	 * Test that Core only boots providers once.
-	 */
-	public function test_core_boots_once_only() {
-		$application = new Application();
-
-		$application->register( ServiceProviderStub::class );
-		$application->boot();
-		$application->boot();
-
-		$this->expectOutputRegex( '/^booted$/' );
-	}
-
-	/**
 	 * Test that the deactivation hook is called.
 	 */
 	public function test_deactivation_hook_is_called() {
 		$plugin_basename = plugin_basename( $this->plugin_file );
 
-		register_deactivation_hook( $plugin_basename, array( omniform()->app, 'deactivation' ) );
+		register_deactivation_hook( $plugin_basename, array( omniform(), 'deactivation' ) );
 		$this->assertTrue( has_filter( 'deactivate_' . $plugin_basename ) );
 
 		do_action( 'deactivate_' . $plugin_basename ); // phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals
@@ -146,12 +79,5 @@ class CoreTest extends \WP_UnitTestCase {
 	public function test_deactivation_hook_removes_maintenance_job() {
 		$plugin_basename = plugin_basename( $this->plugin_file );
 		$this->assertTrue( has_filter( 'deactivate_' . $plugin_basename ) );
-	}
-}
-
-// phpcs:disable
-class ServiceProviderStub extends ServiceProvider {
-	public function boot() {
-		echo 'booted';
 	}
 }
