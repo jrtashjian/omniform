29f9453c2af9f8a025d95d528b6de2073cc1c332
JR Tashjian <hello@jrtashjian.com>
2023-04-02 18:33:11 -0400

    Cleanup attributes with supports

--- packages/block-library/textarea/edit.js
+++ packages/block-library/textarea/edit.js
@@ -6,42 +6,29 @@ import {
 	RichText,
 	useBlockProps,
 } from '@wordpress/block-editor';
-import {
-	ResizableBox,
-} from '@wordpress/components';
 
 const Edit = ( {
-	attributes: { fieldPlaceholder, height },
+	attributes: { fieldPlaceholder },
 	setAttributes,
 	isSelected,
 } ) => {
 	const blockProps = useBlockProps();
 
 	return (
-		<ResizableBox
-			size={ { height: height || 'auto' } }
-			minHeight="24"
-			showHandle={ isSelected }
-			enable={ { bottom: true } }
-			onResizeStop={ ( _event, _direction, elt ) => {
-				setAttributes( { height: parseInt( elt.style.height ) } );
-			} }
-		>
-			<RichText
-				{ ...blockProps }
-				identifier="fieldControl"
-				aria-label={ __( 'Placeholder text for text input.', 'omniform' ) }
-				placeholder={
-					( fieldPlaceholder || ! isSelected )
-						? undefined
-						: __( 'Enter a placeholder…', 'omniform' )
-				}
-				value={ fieldPlaceholder }
-				onChange={ ( html ) => setAttributes( { fieldPlaceholder: html } ) }
-				withoutInteractiveFormatting
-				allowedFormats={ [] }
-			/>
-		</ResizableBox>
+		<RichText
+			{ ...blockProps }
+			identifier="fieldControl"
+			aria-label={ __( 'Placeholder text for text input.', 'omniform' ) }
+			placeholder={
+				( fieldPlaceholder || ! isSelected )
+					? undefined
+					: __( 'Enter a placeholder…', 'omniform' )
+			}
+			value={ fieldPlaceholder }
+			onChange={ ( html ) => setAttributes( { fieldPlaceholder: html } ) }
+			withoutInteractiveFormatting
+			allowedFormats={ [] }
+		/>
 	);
 };
 
