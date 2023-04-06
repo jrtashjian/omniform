584593413c0f637063e6ff690a6870c67b0f1c64
JR Tashjian <hello@jrtashjian.com>
2023-04-05 22:48:54 -0400

    Extend Select from BaseControlBlock

--- includes/BlockLibrary/Blocks/Select.php
+++ includes/BlockLibrary/Blocks/Select.php
@@ -10,41 +10,16 @@ namespace OmniForm\BlockLibrary\Blocks;
 /**
  * The Select block class.
  */
-class Select extends BaseBlock {
+class Select extends BaseControlBlock {
 	/**
-	 * Renders the block on the server.
+	 * Renders the form control.
 	 *
 	 * @return string
 	 */
-	protected function render() {
-		$placeholder_option = '';
-
-		if ( ! $this->getBlockAttribute( 'isMultiple' ) ) {
-			$placeholder_option = sprintf(
-				'<option value="">%s</option>',
-				esc_attr( $this->getBlockAttribute( 'fieldPlaceholder' ) )
-			);
-		}
-
-		$extra_attributes = array_filter(
-			array(
-				'id'       => sanitize_title( $this->getBlockContext( 'omniform/fieldName' ) ),
-				'name'     => sanitize_title( $this->getBlockContext( 'omniform/fieldName' ) ),
-				'multiple' => $this->getBlockAttribute( 'isMultiple' ),
-			)
-		);
-
-		// Detect minHeight attribute so we can use it to set the height of the select element.
-		$attributes = \WP_Block_Supports::get_instance()->apply_block_supports();
-
-		if ( ! empty( $attributes ) && array_key_exists( 'style', $attributes ) ) {
-			if ( $this->getBlockAttribute( 'isMultiple' ) && preg_match( '/min-height:([^;]+);/', $attributes['style'], $matches ) ) {
-				$extra_attributes['style'] = esc_attr( sprintf( 'height: %s;', $matches[1] ) );
-			}
-		}
-
+	public function renderControl() {
 		// Remove minHeight attribute so it doesn't get added to the wrapper element if not a multiple select.
-		$block_attributes = get_block_wrapper_attributes( $extra_attributes );
+		$block_attributes = get_block_wrapper_attributes( $this->getExtraWrapperAttributes() );
+
 		if ( ! $this->getBlockAttribute( 'isMultiple' ) ) {
 			$block_attributes = preg_replace( '/min-height:([^;]+);/', '', $block_attributes );
 		}
@@ -52,7 +27,52 @@ class Select extends BaseBlock {
 		return sprintf(
 			'<select %s>%s</select>',
 			$block_attributes,
-			$placeholder_option . $this->content
+			$this->content
 		);
+	}
+
+	/**
+	 * Gets the extra wrapper attributes for the field to be passed into get_block_wrapper_attributes().
+	 *
+	 * @return array
+	 */
+	public function getExtraWrapperAttributes() {
+		$extra_attributes = wp_parse_args(
+			array(
+				'multiple' => $this->getBlockAttribute( 'isMultiple' ),
+				'style'    => $this->getElementHeight(),
+			),
+			parent::getExtraWrapperAttributes()
+		);
+
+		return array_filter( $extra_attributes );
+	}
+
+	/**
+	 * Gets the control's name attribute.
+	 *
+	 * @return string
+	 */
+	public function getControlName() {
+		return $this->getBlockAttribute( 'isMultiple' )
+			? parent::getControlName() . '[]'
+			: parent::getControlName();
+	}
+
+	/**
+	 * Gets the height of the select element (derived from the minHeight attribute).
+	 *
+	 * @return string|false
+	 */
+	private function getElementHeight() {
+		$attributes = \WP_Block_Supports::get_instance()->apply_block_supports();
+
+		if ( ! empty( $attributes ) && array_key_exists( 'style', $attributes ) ) {
+			if ( $this->getBlockAttribute( 'isMultiple' ) && preg_match( '/min-height:([^;]+);/', $attributes['style'], $matches ) ) {
+				return sprintf( 'height: %s;', $matches[1] );
+			}
+		}
+
+		return false;
 	}
 }
