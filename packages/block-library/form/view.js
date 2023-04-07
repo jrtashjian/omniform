2d3548c6bc5dc400eb52cef0cb05273523ee38b1
JR Tashjian <hello@jrtashjian.com>
2023-04-07 03:07:37 -0400

    Complete the submission handler

--- packages/block-library/form/view.js
+++ packages/block-library/form/view.js
@@ -14,25 +14,46 @@ import apiFetch from '@wordpress/api-fetch';
 			const { action: url, method } = formElement;
 			const body = new FormData( formElement );
 
-			const debug = { url, method, data: {} };
-			for ( const pair of body.entries() ) {
-				debug.data[ pair[ 0 ] ] = pair[ 1 ];
-			}
-			console.debug( 'debug', debug );
+			const messageContainer = event.target.querySelector( '.omniform-response-container' );
 
 			apiFetch( {
 				url,
 				method,
 				body,
-			} ).then( ( response ) => {
-				document.getElementsByClassName( 'omniform-response-message' )[ 0 ].innerText = 'Thank you for signing up!';
-				console.debug( 'response', response );
+			} ).then( () => {
+				messageContainer.innerHTML = '';
+				messageContainer.append( createParagraph( 'Thank you for signing up!' ) );
+
+				// Reset the form.
+				event.target.reset();
 			} ).catch( ( error ) => {
-				document.getElementsByClassName( 'omniform-response-message' )[ 0 ].innerText = 'Something went wrong...';
-				console.debug( 'error', error );
+				messageContainer.innerHTML = '';
+				messageContainer.append( createParagraph( 'Something went wrong...' ) );
+				messageContainer.append( createUnorderedList( Object.values( error.invalid_fields ) ) );
 			} );
 		};
 
+		// Create a paragraph element from text.
+		const createParagraph = ( text ) => {
+			const paragraph = document.createElement( 'p' );
+			paragraph.textContent = text;
+			return paragraph;
+		};
+
+		// Create an unordered list from an array of items.
+		const createUnorderedList = ( list ) => {
+			const listElement = document.createElement( 'ul' );
+
+			list.forEach( ( item ) => {
+				const listItem = document.createElement( 'li' );
+				listItem.textContent = item;
+				listElement.appendChild( listItem );
+			} );
+
+			return listElement;
+		};
+
+		// Add event listeners to all omniforms.
 		document.querySelectorAll( 'form.wp-block-omniform-form' )
 			.forEach( ( form ) => form.addEventListener( 'submit', formResponseHandler ) );
 	} );
