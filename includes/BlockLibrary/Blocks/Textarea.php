4ff5a71dbc09e0953cc539c4083affc14aa8f597
JR Tashjian <hello@jrtashjian.com>
2023-09-19 23:24:22 -0400

    Textarea placeholder should not be the value

--- includes/BlockLibrary/Blocks/Textarea.php
+++ includes/BlockLibrary/Blocks/Textarea.php
@@ -20,7 +20,24 @@ class Textarea extends BaseControlBlock {
 		return sprintf(
 			'<textarea %s>%s</textarea>',
 			get_block_wrapper_attributes( $this->get_extra_wrapper_attributes() ),
-			esc_textarea( $this->get_block_attribute( 'fieldPlaceholder' ) )
+			esc_textarea( $this->get_block_attribute( 'fieldValue' ) )
 		);
+	}
+
+	/**
+	 * Gets the extra wrapper attributes for the field to be passed into get_block_wrapper_attributes().
+	 *
+	 * @return array
+	 */
+	public function get_extra_wrapper_attributes() {
+		$extra_attributes = wp_parse_args(
+			array(
+				'placeholder' => $this->get_block_attribute( 'fieldPlaceholder' ),
+				'aria-label'  => esc_attr( wp_strip_all_tags( $this->get_field_label() ) ),
+			),
+			parent::get_extra_wrapper_attributes()
+		);
+
+		return array_filter( $extra_attributes );
 	}
 }
