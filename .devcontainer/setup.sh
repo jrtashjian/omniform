05466727ea85fa449c6452f267e160ea24f4c0f9
JR Tashjian <hello@jrtashjian.com>
2024-05-05 20:19:36 +0000

    DevContainer: use docker-compose instead of wp-env

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -2,25 +2,30 @@
 
 set -eux
 
-#  If CODESPACE_NAME is set, set SITE_HOST.
-if [ -n ${CODESPACE_NAME+x} ]; then
-	SITE_HOST="https://${CODESPACE_NAME}-8888.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}"
-	echo "{\"config\": {\"WP_SITEURL\": \"${SITE_HOST}\",\"WP_HOME\": \"${SITE_HOST}\"}}" | jq . > /workspaces/omniform/.wp-env.override.json
+if [ -z ${CODESPACE_NAME+x} ]; then
+	SITE_HOST="http://localhost:8080"
+else
+	SITE_HOST="https://${CODESPACE_NAME}-8080.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}"
 fi
 
 echo "PATH=\"\$PATH:${HOME}/.composer/vendor/bin\"" >> $HOME/.bashrc
 export PATH="$PATH:$HOME/.composer/vendor/bin"
 
-# Install PHP Scoper globally
-composer global require humbug/php-scoper
-
-# Install dependencies
 cd /workspaces/omniform
 
+# Prefix required Composer packages.
 composer install --no-autoloader --no-dev --prefer-dist
 php-scoper add-prefix --force
+
+# Reset and install Composer dependencies.
 rm -rf ./vendor
-
 composer install
-
 npm install && npm run build
+
+# Install WordPress.
+cd /var/www/html
+echo "Setting up WordPress at $SITE_HOST"
+
+wp core install --url="$SITE_HOST" \
+    --title="OmniForm Development" \
+    --admin_user="admin" --admin_email="admin@example.com" --admin_password="password" --skip-email
