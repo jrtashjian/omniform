edac0c92f5dd6af7c1eb3a5b78893007b217aa0a
JR Tashjian <hello@jrtashjian.com>
2024-08-09 12:33:08 -0400

    Utilize new hook for submission settings panel

--- packages/components/form-settings/submission-method.js
+++ packages/components/form-settings/submission-method.js
@@ -2,51 +2,20 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { useEntityProp } from '@wordpress/core-data';
 import { PanelBody, TextControl, SelectControl } from '@wordpress/components';
 import { PluginDocumentSettingPanel } from '@wordpress/edit-post';
 import { useSelect } from '@wordpress/data';
 import { store as editorStore } from '@wordpress/editor';
 
-/**
- * Internal dependencies
- */
-import { POST_TYPE } from '../../block-library/shared/constants';
-
 export default function SubmissionMethodSettings( {
-	formId,
 	isDocumentPanel,
+	getSetting,
+	setSetting,
 } ) {
 	const formTypes = useSelect( ( select ) => {
 		const editorSettings = select( editorStore ).getEditorSettings();
 		return editorSettings.omniformFormTypes ?? [];
 	}, [] );
-
-	const [ formType, setFormType ] = useEntityProp( 'postType', POST_TYPE, 'omniform_type', formId );
-	const [ meta, setMeta ] = useEntityProp( 'postType', POST_TYPE, 'meta', formId );
-
-	const metaSubmitMethod = meta?.submit_method;
-	const updateMetaSubmitMethod = ( newValue ) => {
-		setMeta( { ...meta, submit_method: newValue } );
-	};
-
-	const metaSubmitAction = meta?.submit_action;
-	const updateMetaSubmitAction = ( newValue ) => {
-		setMeta( { ...meta, submit_action: newValue } );
-	};
-
-	const updateFormType = ( newValue ) => {
-		setFormType( newValue );
-
-		// Reset the meta values when switching to standard form type.
-		if ( newValue === 'uncategorized' ) {
-			setMeta( {
-				...meta,
-				submit_method: '',
-				submit_action: '',
-			} );
-		}
-	};
 
 	const PanelComponent = isDocumentPanel
 		? PluginDocumentSettingPanel
@@ -59,8 +28,8 @@ export default function SubmissionMethodSettings( {
 		>
 			<SelectControl
 				label={ __( 'Type', 'omniform' ) }
-				value={ formType.toString() || 'uncategorized' }
-				onChange={ updateFormType }
+				value={ getSetting( 'form_type' ) || 'standard' }
+				onChange={ ( newValue ) => setSetting( 'form_type', newValue ) }
 				options={ formTypes.map( ( item ) => ( {
 					label: item.label,
 					value: item.type,
@@ -68,18 +37,18 @@ export default function SubmissionMethodSettings( {
 				help={ __( 'Determines how the form data is processed and submitted.', 'omniform' ) }
 			/>
 
-			{ formType === 'custom' && (
+			{ getSetting( 'form_type' ) === 'custom' && (
 				<>
 					<TextControl
 						label={ __( 'Submit Action', 'omniform' ) }
-						value={ metaSubmitAction }
-						onChange={ updateMetaSubmitAction }
+						value={ getSetting( 'submit_action' ) || '' }
+						onChange={ ( newValue ) => setSetting( 'submit_action', newValue ) }
 						help={ __( 'Enter the URL where the form data will be submitted.', 'omniform' ) }
 					/>
 					<SelectControl
 						label={ __( 'Submit Method', 'omniform' ) }
-						value={ metaSubmitMethod }
-						onChange={ updateMetaSubmitMethod }
+						value={ getSetting( 'submit_method' ) || 'POST' }
+						onChange={ ( newValue ) => setSetting( 'submit_method', newValue ) }
 						options={ [
 							{ label: 'POST', value: 'POST' },
 							{ label: 'GET', value: 'GET' },
