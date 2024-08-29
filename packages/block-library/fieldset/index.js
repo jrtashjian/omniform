d56945f03267ae27839512e213eafd760956bb4b
JR Tashjian <hello@jrtashjian.com>
2024-08-29 17:44:11 -0400

    remove example from fieldset and enable variations

--- packages/block-library/fieldset/index.js
+++ packages/block-library/fieldset/index.js
@@ -1,7 +1,6 @@
 /**
  * WordPress dependencies
  */
-import { __ } from '@wordpress/i18n';
 import { registerBlockType } from '@wordpress/blocks';
 import { decodeEntities } from '@wordpress/html-entities';
 
@@ -11,7 +10,7 @@ import { decodeEntities } from '@wordpress/html-entities';
 import json from './block.json';
 import Edit from './edit';
 import Save from './save';
-// import variations from './variations';
+import variations from './variations';
 import { fieldGroup } from '../shared/icons';
 
 import './style.scss';
@@ -22,47 +21,7 @@ registerBlockType( name, {
 	edit: Edit,
 	icon: { foreground: '#D92E83', src: fieldGroup },
 	save: Save,
-	// variations,
-	example: {
-		attributes: {
-			fieldLabel: __( 'Field Group', 'omniform' ),
-		},
-		innerBlocks: [
-			{
-				attributes: {
-					fieldLabel: __( 'Option One', 'omniform' ),
-					layout: { type: 'flex', orientation: 'horizontal', justifyContent: 'left' },
-				},
-				name: 'omniform/field',
-				innerBlocks: [
-					{ name: 'omniform/input', attributes: { fieldType: 'radio' } },
-					{ name: 'omniform/label' },
-				],
-			},
-			{
-				attributes: {
-					fieldLabel: __( 'Option Two', 'omniform' ),
-					layout: { type: 'flex', orientation: 'horizontal', justifyContent: 'left' },
-				},
-				name: 'omniform/field',
-				innerBlocks: [
-					{ name: 'omniform/input', attributes: { fieldType: 'radio' } },
-					{ name: 'omniform/label' },
-				],
-			},
-			{
-				attributes: {
-					fieldLabel: __( 'Option Three', 'omniform' ),
-					layout: { type: 'flex', orientation: 'horizontal', justifyContent: 'left' },
-				},
-				name: 'omniform/field',
-				innerBlocks: [
-					{ name: 'omniform/input', attributes: { fieldType: 'radio' } },
-					{ name: 'omniform/label' },
-				],
-			},
-		],
-	},
+	variations,
 	// Get block name from the option value.
 	__experimentalLabel: ( { fieldLabel } ) => fieldLabel && decodeEntities( fieldLabel ),
 } );
