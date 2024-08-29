79cffa6596223835176cdaad2862a9c3d2479c49
JR Tashjian <hello@jrtashjian.com>
2024-08-29 17:43:45 -0400

    Allow lists to be pasted into an select-option block

--- packages/block-library/select-option/edit.js
+++ packages/block-library/select-option/edit.js
@@ -5,8 +5,11 @@ import { __ } from '@wordpress/i18n';
 import {
 	RichText,
 	useBlockProps,
+	store as blockEditorStore,
 } from '@wordpress/block-editor';
 import { useMergeRefs } from '@wordpress/compose';
+import { createBlock } from '@wordpress/blocks';
+import { useDispatch, useSelect } from '@wordpress/data';
 
 /**
  * Internal dependencies
@@ -25,10 +28,32 @@ const Edit = ( props ) => {
 		fieldLabel,
 	} = attributes;
 
+	const { insertBlocks } = useDispatch( blockEditorStore );
+	const { getBlockRootClientId, getBlockIndex } = useSelect( blockEditorStore );
+
 	const blockProps = useBlockProps( {
 		ref: useMergeRefs( [ useEnter( clientId ) ] ),
 		className: 'omniform-field-select-option',
 	} );
+
+	const onChangeFieldLabel = ( value ) => {
+		// Handle the case where multiple lines are pasted.
+		const lines = value.split( '<br>' ).filter( ( line ) => line.trim() );
+
+		setAttributes( { fieldLabel: lines.shift() } );
+
+		const blocks = lines.map( ( line ) =>
+			createBlock( 'omniform/select-option', { fieldLabel: line } )
+		);
+
+		if ( blocks.length ) {
+			insertBlocks(
+				blocks,
+				getBlockIndex( clientId ) + 1,
+				getBlockRootClientId( clientId )
+			);
+		}
+	};
 
 	return (
 		<div { ...blockProps }>
@@ -38,7 +63,7 @@ const Edit = ( props ) => {
 				aria-label={ __( 'Help text', 'omniform' ) }
 				placeholder={ __( 'Write the option text…', 'omniform' ) }
 				value={ fieldLabel }
-				onChange={ ( html ) => setAttributes( { fieldLabel: html } ) }
+				onChange={ onChangeFieldLabel }
 				withoutInteractiveFormatting
 				allowedFormats={ [] }
 				disableLineBreaks
