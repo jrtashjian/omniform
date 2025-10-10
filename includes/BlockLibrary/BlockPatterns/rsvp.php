5b68c6c6df9dd280693916e66a975ca27cdb438f
JR Tashjian <hello@jrtashjian.com>
2025-10-09 20:19:13 -0400

    Remove fieldName attributes from form fields in block patterns for consistency

--- includes/BlockLibrary/BlockPatterns/rsvp.php
+++ includes/BlockLibrary/BlockPatterns/rsvp.php
@@ -13,27 +13,27 @@ return array(
 
 		<!-- wp:omniform/response-notification {"messageContent":"' . esc_html__( 'Unfortunately, your submission was not successful. Please ensure all fields are correctly filled out and try again.', 'omniform' ) . '","className":"is-style-error"} /-->
 
-		<!-- wp:omniform/field {"fieldLabel":"' . esc_html__( 'Name', 'omniform' ) . '","fieldName":"' . esc_html__( 'name', 'omniform' ) . '"} -->
+		<!-- wp:omniform/field {"fieldLabel":"' . esc_html__( 'Name', 'omniform' ) . '"} -->
 		<!-- wp:omniform/label /-->
 
 		<!-- wp:omniform/input /-->
 		<!-- /wp:omniform/field -->
 
-		<!-- wp:omniform/field {"fieldLabel":"' . esc_html__( 'E-mail', 'omniform' ) . '","fieldName":"' . esc_html__( 'e-mail', 'omniform' ) . '"} -->
+		<!-- wp:omniform/field {"fieldLabel":"' . esc_html__( 'E-mail', 'omniform' ) . '"} -->
 		<!-- wp:omniform/label /-->
 
 		<!-- wp:omniform/input {"fieldType":"email"} /-->
 		<!-- /wp:omniform/field -->
 
-		<!-- wp:omniform/fieldset {"fieldLabel":"' . esc_html__( 'Attending?', 'omniform' ) . '","fieldName":"' . esc_html__( 'attending', 'omniform' ) . '"} -->
+		<!-- wp:omniform/fieldset {"fieldLabel":"' . esc_html__( 'Attending?', 'omniform' ) . '"} -->
 		<!-- wp:group {"style":{"spacing":{"padding":{"top":"0.75em","bottom":"0.75em"}}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
-		<div class="wp-block-group" style="padding-top:0.75em;padding-bottom:0.75em"><!-- wp:omniform/field {"fieldLabel":"' . esc_html__( 'Joyfully accepts', 'omniform' ) . '","fieldName":"' . esc_html__( 'joyfully-accepts', 'omniform' ) . '","className":"is-style-inline"} -->
+		<div class="wp-block-group" style="padding-top:0.75em;padding-bottom:0.75em"><!-- wp:omniform/field {"fieldLabel":"' . esc_html__( 'Joyfully accepts', 'omniform' ) . '","className":"is-style-inline"} -->
 		<!-- wp:omniform/input {"fieldType":"radio"} /-->
 
 		<!-- wp:omniform/label /-->
 		<!-- /wp:omniform/field -->
 
-		<!-- wp:omniform/field {"fieldLabel":"' . esc_html__( 'Regretfully declines', 'omniform' ) . '","fieldName":"' . esc_html__( 'regretfully-declines', 'omniform' ) . '","className":"is-style-inline"} -->
+		<!-- wp:omniform/field {"fieldLabel":"' . esc_html__( 'Regretfully declines', 'omniform' ) . '","className":"is-style-inline"} -->
 		<!-- wp:omniform/input {"fieldType":"radio"} /-->
 
 		<!-- wp:omniform/label /-->
@@ -41,7 +41,7 @@ return array(
 		<!-- /wp:group -->
 		<!-- /wp:omniform/fieldset -->
 
-		<!-- wp:omniform/field {"fieldLabel":"' . esc_html__( 'Number of persons', 'omniform' ) . '","fieldName":"' . esc_html__( 'number-of-persons', 'omniform' ) . '"} -->
+		<!-- wp:omniform/field {"fieldLabel":"' . esc_html__( 'Number of persons', 'omniform' ) . '"} -->
 		<!-- wp:omniform/label /-->
 
 		<!-- wp:omniform/input {"fieldType":"number"} /-->
