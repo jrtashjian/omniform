b57627596ea3b928e5695622cd70ccea0a31f1d4
JR Tashjian <hello@jrtashjian.com>
2025-10-20 18:43:07 -0400

    Add unit tests for CallbackSupport trait

---
+++ phpunit/unit/CallbackSupportTest.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Tests the CallbackSupport trait.
+ *
+ * @package OmniForm
+ */
+
+namespace OmniForm\Tests\Unit;
+
+use WP_Mock;
+
+/**
+ * Test class for CallbackSupport trait.
+ */
+class CallbackSupportTest extends BaseTestCase {
+	use \OmniForm\Traits\CallbackSupport;
+
+	/**
+	 * Test the has_callback method.
+	 */
+	public function test_has_callback() {
+		$this->assertFalse( $this->has_callback( 'not a valid callback' ) );
+		$this->assertTrue( $this->has_callback( '{{valid_callback}}' ) );
+	}
+
+	/**
+	 * Data provider for test_process_callbacks.
+	 */
+	public static function provideCallbackData() {
+		return array(
+			array( 'no callbacks here', 'no callbacks here' ),
+			array( '{{omniform_existent_callback_return_string}}', 'callback string' ),
+			array( '{{omniform_existent_callback_return_array}}', '' ),
+			array( '{{omniform_existent_callback_return_object}}', '' ),
+			array( '{{omniform_existent_callback_return_false}}', '0' ),
+			array( '{{omniform_existent_callback_return_true}}', '1' ),
+			array( '{{omniform_existent_callback_return_number}}', '42' ),
+			array( 'text before {{omniform_existent_callback_return_string}} text after', 'text before callback string text after' ),
+			array( '{{omniform_existent_callback_return_string}} and {{omniform_existent_callback_return_number}}', 'callback string and 42' ),
+			array( '{{omniform_existent_callback_return_string}} and {{omniform_existent_callback_return_true}}', 'callback string and 1' ),
+			array( 'invalid {{callback', 'invalid {{callback' ),
+			array( 'invalid {{non_existent_callback}}', 'invalid ' ),
+		);
+	}
+
+	/**
+	 * Test the process_callbacks method.
+	 *
+	 * @dataProvider provideCallbackData
+	 *
+	 * @param string $input    The input string to process.
+	 * @param string $expected The expected output after processing callbacks.
+	 */
+	public function test_process_callbacks( $input, $expected ) {
+		WP_Mock::userFunction(
+			'omniform_existent_callback_return_string',
+			array( 'return' => 'callback string' )
+		);
+
+		WP_Mock::userFunction(
+			'omniform_existent_callback_return_array',
+			array( 'return' => array() )
+		);
+
+		WP_Mock::userFunction(
+			'omniform_existent_callback_return_object',
+			array( 'return' => new \stdClass() )
+		);
+
+		WP_Mock::userFunction(
+			'omniform_existent_callback_return_true',
+			array( 'return' => true )
+		);
+
+		WP_Mock::userFunction(
+			'omniform_existent_callback_return_false',
+			array( 'return' => false )
+		);
+
+		WP_Mock::userFunction(
+			'omniform_existent_callback_return_number',
+			array( 'return' => 42 )
+		);
+
+		$result = $this->process_callbacks( $input );
+		$this->assertIsString( $result );
+		$this->assertEquals( $expected, $result );
+	}
+}
