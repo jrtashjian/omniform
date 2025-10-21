51224db796e6ffa164b31b924ee168e3f8354ace
JR Tashjian <hello@jrtashjian.com>
2025-10-20 22:01:47 -0400

    Add data provider for render tests in LabelTest class

--- phpunit/unit/BlockLibrary/Blocks/LabelTest.php
+++ phpunit/unit/BlockLibrary/Blocks/LabelTest.php
@@ -49,36 +49,73 @@ class LabelTest extends BaseBlockTestCase {
 	}
 
 	/**
-	 * Test render.
+	 * Data provider for render tests.
 	 */
-	public function test_render() {
+	public function provide_render_scenarios() {
+		return array(
+			'required_field'     => array(
+				array(
+					'context'               => array(
+						'omniform/fieldLabel'      => 'Field Label',
+						'omniform/fieldName'       => 'field_name',
+						'omniform/fieldIsRequired' => true,
+					),
+					'expected_contains'     => array( '<label for="field_name" class="label">', 'Field Label<abbr' ),
+					'expected_not_contains' => array(),
+					'is_required'           => true,
+				),
+			),
+			'not_required_field' => array(
+				array(
+					'context'               => array(
+						'omniform/fieldLabel'      => 'Field Label',
+						'omniform/fieldName'       => 'field_name',
+						'omniform/fieldIsRequired' => false,
+					),
+					'expected_contains'     => array( '<label for="field_name" class="label">', 'Field Label</label>' ),
+					'expected_not_contains' => array( '<abbr' ),
+					'is_required'           => false,
+				),
+			),
+			'no_label'           => array(
+				array(
+					'context'         => array(),
+					'expected_result' => '',
+					'is_required'     => false,
+				),
+			),
+		);
+	}
+
+	/**
+	 * Test render.
+	 *
+	 * @dataProvider provide_render_scenarios
+	 * @param array $scenario The test scenario data.
+	 */
+	public function test_render( $scenario ) {
 		\WP_Mock::userFunction( 'get_block_wrapper_attributes' )->andReturn( 'class="label"' );
 
-		$block = $this->createBlockWithContext(
-			array(
-				'omniform/fieldLabel'      => 'Field Label',
-				'omniform/fieldName'       => 'field_name',
-				'omniform/fieldIsRequired' => true,
-			)
-		);
+		if ( $scenario['is_required'] ) {
+			$mock_form = \Mockery::mock();
+			$mock_form->shouldReceive( 'get_required_label' )->andReturn( '*' );
+			$mock_app = \Mockery::mock();
+			$mock_app->shouldReceive( 'get' )->andReturn( $mock_form );
+			\WP_Mock::userFunction( 'omniform' )->andReturn( $mock_app );
+		}
 
-		$mock_form = \Mockery::mock();
-		$mock_form->shouldReceive( 'get_required_label' )->andReturn( '*' );
-		$mock_app = \Mockery::mock();
-		$mock_app->shouldReceive( 'get' )->andReturn( $mock_form );
-		\WP_Mock::userFunction( 'omniform' )->andReturn( $mock_app );
+		$block  = $this->createBlockWithContext( $scenario['context'] );
+		$result = $this->block->render_block( array(), '', $block );
 
-		$result = $this->block->render_block(
-			array(),
-			'',
-			$block
-		);
-		$this->assertStringContainsString( '<label for="field_name" class="label">', $result );
-		$this->assertStringContainsString( 'Field Label<abbr', $result );
-
-		// No label.
-		$block2 = $this->createBlockWithContext();
-		$result = $this->block->render_block( array(), '', $block2 );
-		$this->assertEquals( '', $result );
+		if ( isset( $scenario['expected_result'] ) ) {
+			$this->assertEquals( $scenario['expected_result'], $result );
+		} else {
+			foreach ( $scenario['expected_contains'] as $str ) {
+				$this->assertStringContainsString( $str, $result );
+			}
+			foreach ( $scenario['expected_not_contains'] as $str ) {
+				$this->assertStringNotContainsString( $str, $result );
+			}
+		}
 	}
 }
