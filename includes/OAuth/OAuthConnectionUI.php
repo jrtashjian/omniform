816267a1d84af89333b3e1f97da1a02b9c791988
JR Tashjian <hello@jrtashjian.com>
2025-10-22 16:59:37 -0400

    Remove debug information

--- includes/OAuth/OAuthConnectionUI.php
+++ includes/OAuth/OAuthConnectionUI.php
@@ -54,59 +54,18 @@ class OAuthConnectionUI {
 	 * @return void
 	 */
 	public function render(): void {
-		$api_client    = $this->container->get( ApiClient::class );
-		$token_storage = $this->container->get( TokenStorage::class );
-
-		$data = array();
-
-		$response = $api_client->get( '/user' );
-
-		if ( ! is_wp_error( $response ) ) {
-			$data = json_decode( wp_remote_retrieve_body( $response ) );
-		}
-
-		$debug_data = array(
-			'client_id'     => $token_storage->get_client_id(),
-			'access_token'  => $token_storage->get_access_token() ? 'set' : 'not set',
-			'refresh_token' => $token_storage->get_refresh_token() ? 'set' : 'not set',
-			'token_expires' => get_option( 'omniform_token_expires' ),
-			'code_verifier' => get_transient( 'omniform_oauth_code_verifier' ) ? 'set' : 'not set',
-		);
+		$this->render_buttons();
 		?>
-		<div id="omniform">
-			<?php $this->render_buttons(); ?>
+		<h3>API Health Check</h3>
+		<?php
+		$healthcheck = $this->get_health_status();
 
-			<h3>API Health Check</h3>
-			<?php
-			$healthcheck = $this->get_health_status();
-
-			$status_message = $healthcheck['healthy']
-				/* translators: %s is the latency time */
-				? sprintf( __( 'The API is available (%s)', 'omniform' ), $healthcheck['latency'] )
-				: sprintf( __( 'The API is unavailable', 'omniform' ) );
-			?>
-			<p><?php echo esc_html( $status_message ); ?></p>
-
-			<h3>API Response</h3>
-			<pre>
-			<?php
-			if ( ! is_wp_error( $response ) ) {
-				echo esc_html( 'Response Code: ' . wp_remote_retrieve_response_code( $response ) . "\n" );
-				echo esc_html( 'Response Body: ' . print_r( json_decode( wp_remote_retrieve_body( $response ), true ), true ) );
-			} else {
-				echo esc_html( 'Error: ' . $response->get_error_message() );
-			}
-			?>
-			</pre>
-
-			<h3>OAuth Debug</h3>
-			<pre>
-			<?php
-			// phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_print_r
-			print_r( $debug_data );
-			?>
-			</pre>
-		</div>
+		$status_message = $healthcheck['healthy']
+			/* translators: %s is the latency time */
+			? sprintf( __( 'The API is available (%s)', 'omniform' ), $healthcheck['latency'] )
+			: sprintf( __( 'The API is unavailable', 'omniform' ) );
+		?>
+		<p><?php echo esc_html( $status_message ); ?></p>
 		<?php
 	}
 
