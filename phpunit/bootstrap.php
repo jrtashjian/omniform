df70cba0f47d3798fb1d72200bacf38adbaa855a
JR Tashjian <hello@jrtashjian.com>
2026-01-09 00:21:19 -0500

    Add error handler to suppress deprecation warnings in composer dependencies.

--- phpunit/bootstrap.php
+++ phpunit/bootstrap.php
@@ -11,5 +11,15 @@ require_once dirname( __DIR__ ) . '/vendor/autoload.php';
 // Initialize WP_Mock.
 WP_Mock::bootstrap();
 
+// Suppress deprecation errors from vendor dependencies.
+set_error_handler(
+	function( $errno, $errstr, $errfile ) {
+		if ( E_DEPRECATED === $errno && false !== strpos( $errfile, '/vendor/' ) ) {
+			return true; // Suppress the error.
+		}
+		return false; // Let default handler deal with it.
+	}
+);
+
 // Activate strict mode.
 WP_Mock::activateStrictMode();
