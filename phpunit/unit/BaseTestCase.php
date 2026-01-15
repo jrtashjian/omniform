918acfe98897bc0015064f48856558177f545822
JR Tashjian <hello@jrtashjian.com>
2026-01-14 19:45:36 -0500

    Enhance BaseTestCase setup with WP_Mock user functions for improved testing environment

--- phpunit/unit/BaseTestCase.php
+++ phpunit/unit/BaseTestCase.php
@@ -7,10 +7,33 @@
 
 namespace OmniForm\Tests\Unit;
 
+use WP_Mock;
 use WP_Mock\Tools\TestCase as WP_Mock_TestCase;
 
 /**
  * Base test case extending WP_Mock TestCase.
  */
 class BaseTestCase extends WP_Mock_TestCase {
+	/**
+	 * Sets up the test environment before each test method is executed.
+	 */
+	public function setUp(): void {
+		parent::setUp();
+
+		WP_Mock::userFunction( 'wp_parse_args' )->andReturnUsing(
+			function ( $args, $defaults ) {
+				return array_merge( $defaults, $args );
+			}
+		);
+
+		WP_Mock::userFunction( 'get_block_wrapper_attributes' )->andReturnUsing(
+			function ( $attrs ) {
+				$parts = array();
+				foreach ( $attrs as $key => $value ) {
+					$parts[] = $key . '="' . $value . '"';
+				}
+				return implode( ' ', $parts );
+			}
+		);
+	}
 }
